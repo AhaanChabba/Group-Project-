f4cead75e3037e6142702299ca80557e1d4e9814
Alisa1098 <118067084+Alisa1098@users.noreply.github.com>
2023-10-27 16:24:26 -0400

    finishing

--- alisa-js-exercise.js
+++ alisa-js-exercise.js
@@ -3,7 +3,9 @@ document.getElementById("b2").addEventListener("click", buttonH4);
 document.getElementById("colorPurple").addEventListener("click", buttonPurple);
 document.getElementById("colorGreen").addEventListener("click", buttonGreen);
 document.getElementById("colorPink").addEventListener("click", buttonPink);
+document.getElementById("submit").addEventListener("click", mirror);
 //console.log(document.getElementById("b1"))
+//add p element that says "you pressed a button!"
 function buttonP (){
     const paragraph = document.createElement("p");
     const node = document.createTextNode("You pressed a button!");
@@ -12,25 +14,36 @@ function buttonP (){
     const element = document.getElementById("div1");
     element.appendChild(paragraph);  
 }
+//function that changes the inner html text 
  function buttonH4 (){
      var gonnaChange = document.getElementById("changing").innerHTML;
-     var change = parseInt(gonnaChange) + 1;
+     var change = parseInt(gonnaChange) + 1; //parseint casts the innerNTML to an int
      document.getElementById("changing").innerHTML = change;
-} 
+}
+//makes para purple 
 function buttonPurple (){
     /*const para = getElementById("colorP");
     const.style.color = purple; */
     var para = document.getElementById("colorP"); 
     para.style.color = "purple";
 }
+//makes para green
 function buttonGreen (){
     var para = document.getElementById("colorP"); 
     para.style.color = "green";
 
 }
+//makes para pink
 function buttonPink (){
     var para = document.getElementById("colorP"); 
     para.style.color = "pink";
+
+}
+function mirror (){
+    const inp = document.getElementById("tbox");
+    const box = document.getElementById("div2");
+   
+    
 
 }
     
